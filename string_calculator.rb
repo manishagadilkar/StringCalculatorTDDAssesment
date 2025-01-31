3d25240486e8430f4c5d033d590b5ba13449d0eb
Manisha Gadilkar <manishagadilkar93@gmail.com>
2025-01-31 09:34:56 +0000

    Added code for #Support different delimiters.

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,6 +8,13 @@ class StringCalculator
 
     # Check for invalid input where a number ends with a comma followed by a newline
     raise 'input is invalid' if numbers.match?(/,\n|\n,/)
+
+    # Check and set custom delimiter if present, otherwise use default delimiters comma and newline
+    if numbers.start_with?('//')
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    else
+      delimiter = /[,|\n]/
+    end
   end
 
   private
@@ -27,6 +34,23 @@ class StringCalculator
     raise "negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
 
     integer_digits
+  end
+
+  def parse_custom_delimiter(numbers)
+    parts = numbers.split("\n", 2) # Split only on first newline
+    custom_delimiter = parts[0][2..]
+    numbers = parts[1]
+
+    # Allow multiple single char or one longer custom delimiter
+    custom_delimiter = Regexp.escape(custom_delimiter)
+    pattern = custom_delimiter.include?('[') ? parse_multiple_delimiters(custom_delimiter) : custom_delimiter
+
+    [pattern, numbers]
+  end
+
+  def parse_multiple_delimiters(custom_delimiter)
+    delimiters = custom_delimiter.scan(/\[([^\[\]]+)\]/).flatten.map { |delim| Regexp.escape(delim) }
+    Regexp.union(delimiters)
   end
 end
 
